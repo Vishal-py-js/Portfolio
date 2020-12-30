41954bbbde4d0b622c55953ed2e7277cebbb2eb1
Vishal Kumar <vskkr007@gmail.com>
2020-12-30 21:10:29 +0530

    Added templates dirs in the settings file

--- Mywebsite/settings.py
+++ Mywebsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATE_DIR'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates/Myprofile')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
